479125fdedd1aa19b6cd82372684963369e88392
leen <leen.kilani@gmail.com>
2021-09-27 11:33:00 +0300

    feat(toy-problem): maximumProfit

--- buyAndSell/index.test.js
+++ buyAndSell/index.test.js
@@ -1,22 +1,22 @@
 /*
-*
-* Given an array representing prices of the stock on different days,
-* find the maximum profit that can be earned by performing maximum of
-* one transaction. A transaction consists of activity of buying and
-* selling the stock on different or same days.
-*
-*/
+ *
+ * Given an array representing prices of the stock on different days,
+ * find the maximum profit that can be earned by performing maximum of
+ * one transaction. A transaction consists of activity of buying and
+ * selling the stock on different or same days.
+ *
+ */
 
 /*
-*
-* For example in this array - [100, 80, 120, 130, 70, 60, 100, 125]
-* the price of the stock on day-1 is 100, on day-2 is 80 and so on.
-* The maximum profit that could be earned in this window is 65
-* (buy at 60 and sell at 125).
-* For price array - [100, 80, 70, 65, 60, 55, 50], maximum profit
-* that could be earned is 0.
-*
-*/
+ *
+ * For example in this array - [100, 80, 120, 130, 70, 60, 100, 125]
+ * the price of the stock on day-1 is 100, on day-2 is 80 and so on.
+ * The maximum profit that could be earned in this window is 65
+ * (buy at 60 and sell at 125).
+ * For price array - [100, 80, 70, 65, 60, 55, 50], maximum profit
+ * that could be earned is 0.
+ *
+ */
 
 // Feel free to add helper functions if needed
 function getMaxOfArray(numArray) {
@@ -26,6 +26,17 @@ function getMinOfArray(numArray) {
   return Math.min.apply(null, numArray);
 }
 
-var maximumProfit  = function(array) {
-	//your code is here
-}
+var maximumProfit = function (array) {
+  var buy_at = getMinOfArray(array);
+  var sell_at = getMaxOfArray(array.slice(array.indexOf(buy_at)));
+  return sell_at - buy_at;
+};
+
+describe("Tests", () => {
+  it("test maximumProfit", () => {
+    expect(maximumProfit([100, 80, 120, 130, 70, 60, 100, 125])).toStrictEqual(
+      65
+    );
+    expect(maximumProfit([100, 80, 70, 65, 60, 55, 50])).toStrictEqual(0);
+  });
+});
