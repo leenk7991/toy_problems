f35b74d54d0583c948afce4eb6634336ad87d19c
leen <leen.kilani@gmail.com>
2021-09-28 11:21:55 +0300

    translateRomanNumeral

--- oldRomanTranslate/index.test.js
+++ oldRomanTranslate/index.test.js
@@ -1,4 +1,3 @@
-
 /**
  * Given a roman numeral as input, write a function that converts the roman
  * numeral to a number and outputs it.
@@ -23,9 +22,26 @@ const DIGIT_VALUES = {
   L: 50,
   C: 100,
   D: 500,
-  M: 1000
+  M: 1000,
 };
 
- const translateRomanNumeral = (romanNumeral)=> {
-   // TO DO ...
+const translateRomanNumeral = (romanNumeral) => {
+  str_arr = romanNumeral.split("").reverse();
+  var num = DIGIT_VALUES[str_arr[0]];
+  const array_length = str_arr.length;
+  for (var i = 1; i < array_length; i++) {
+    if (DIGIT_VALUES[str_arr[i]] >= DIGIT_VALUES[str_arr[i - 1]]) {
+      num += DIGIT_VALUES[str_arr[i]];
+    } else {
+      num -= DIGIT_VALUES[str_arr[i]];
+    }
   }
+  return num;
+};
+
+describe("Tests", () => {
+  it("test translateRomanNumeral", () => {
+    expect(translateRomanNumeral("XXI")).toStrictEqual(21);
+    expect(translateRomanNumeral("IV")).toStrictEqual(4);
+  });
+});
