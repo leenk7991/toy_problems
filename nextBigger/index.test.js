4ac34b10e12c9ae5433d5feeb3ad9bc6a2efb932
leen <leen.kilani@gmail.com>
2021-10-13 11:15:09 +0300

    nextBigger

--- nextBigger/index.test.js
+++ nextBigger/index.test.js
@@ -13,6 +13,20 @@ nextBigger(111)==-1
 nextBigger(531)==-1
 */
 
-const nextBigger = function(num) {
-
+const nextBigger = function (num) {
+  let digits = num.toString().split("");
+  digits.sort().reverse();
+  let res = parseInt(digits.join(""));
+  return res > num ? res : -1;
 };
+
+describe("Tests", () => {
+  it("test nextBigger", () => {
+    expect(nextBigger(12)).toStrictEqual(21);
+    expect(nextBigger(513)).toEqual(531);
+    expect(nextBigger(2071)).toEqual(7210);
+    expect(nextBigger(9)).toEqual(-1);
+    expect(nextBigger(111)).toEqual(-1);
+    expect(nextBigger(531)).toEqual(-1);
+  });
+});
