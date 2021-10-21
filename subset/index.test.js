37243784c23e2bb5acd0959429bb48dad445cfe3
leen <leen.kilani@gmail.com>
2021-10-21 11:49:38 +0300

    subset

--- subset/index.test.js
+++ subset/index.test.js
@@ -16,8 +16,7 @@
  *
  * See http://en.wikipedia.org/wiki/Subset for more on the definition of a
  * subset.
-*/
-
+ */
 
 /*
  * Extra credit:
@@ -25,6 +24,18 @@ Make the method work for arrays that contain objects and/or arrays as elements.
 */
 
 // eslint-disable-next-line no-extend-native
-Array.prototype.isSubsetOf = function(arr) {
-
+Array.prototype.isSubsetOf = function (arr) {
+  let is_subset = false;
+  this.forEach((el) => {
+    is_subset = arr.indexOf(el) < 0 ? false : true;
+  });
+  return is_subset;
 };
+let a = ["c", "d"];
+console.log(a.isSubsetOf(["b", "c", "d"])); //true
+let map1 = { name: "potato" };
+let map2 = { name: "tomato" };
+let map3 = { name: "sugar" };
+let b = [1, 2, ["another", "array"], map1];
+console.log(b.isSubsetOf([2, map2, map1])); //true
+console.log(b.isSubsetOf([5, 6, map3])); //false
